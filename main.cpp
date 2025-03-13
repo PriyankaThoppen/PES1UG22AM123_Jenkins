ae74213cc5d19cb39023cced10b983b88ea5a117
PriyankaThoppen <mail2prixi@gmail.com>
2025-03-13 16:15:03 +0530

    Update and rename hello.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello Task 5 Pipeline PES1UG22AM123";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  cout << "added a webhook!";
+
 
   return 0;
 }
