895919d28577b2e182cd36e1f2202bb11370a734
PriyankaThoppen <mail2prixi@gmail.com>
2025-03-13 16:07:01 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello Task 5 Pipeline PES1UG22AM123";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "added a webhook!";
+
+  return 0;
+}
